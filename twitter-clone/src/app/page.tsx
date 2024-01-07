e9f468596378a4d6fcc1d1e5b9068baa5f5e37f8
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-07 15:16:01 -0400

    Finishing bottom section in the first column

--- twitter-clone/src/app/page.tsx
+++ twitter-clone/src/app/page.tsx
@@ -1,6 +1,6 @@
 import { BiHomeCircle, BiUser } from 'react-icons/bi';
 import { HiOutlineHashtag } from 'react-icons/hi';
-import { BsBell, BsBookmark, BsTwitter, BsEnvelope } from 'react-icons/bs';
+import { BsBell, BsBookmark, BsTwitter, BsEnvelope, BsThreeDots } from 'react-icons/bs';
 import Link from 'next/link';
 
 const NAVIGATION_ITEMS = [
@@ -39,8 +39,8 @@ const Home = () => {
     <div className="w-full h-full flex justify-center items-center relative bg-black">
       <div className="max-w-screen-xl w-full h-full flex relative">
         {/* Left sidebar for navigation/header */}
-        <section className="fixed w-[275px] flex flex-col items-stretch h-screen space-y-4 my-4">
-          <div className="flex flex-col items-stretch h-screen space-y-4 my-4">
+        <section className="fixed w-[275px] flex flex-col items-stretch h-screen">
+          <div className="flex flex-col items-stretch h-full space-y-4 mt-4">
               {NAVIGATION_ITEMS.map((item) => (
                 <Link 
                   className="hover:bg-white/10 text-2xl transition duration-200 flex items-center justify-start w-fit space-x-4 rounded-3xl py-2 px-6"
@@ -57,7 +57,19 @@ const Home = () => {
                 Tweet
               </button>
           </div>
-          <div className="">At the bottom</div>      
+          <button className="rounded-full flex items-center space-x-2 m-4 bg-transparent p-4 text-center hover:bg-white/10 transition duration-200 w-full justify-between">
+            <div className="flex items-center space-x-2">
+              <div className="rounded-full bg-slate-400 w-10 h-10">
+              </div>
+              <div className="text-left text-sm">
+                <div className="font-semibold">UserName</div>
+                <div className="">@username</div>
+              </div>
+            </div>
+            <div>
+              <BsThreeDots />
+            </div>
+          </button>
         </section>
         {/* Main content 
         <main>Home timeline</main>
