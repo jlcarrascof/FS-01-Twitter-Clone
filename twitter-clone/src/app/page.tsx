9880c11626c380a41e7f290f0d70704d037526dc
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-07 10:56:22 -0400

    First configuration styles using Tailwind CSS - Home Page

--- twitter-clone/src/app/page.tsx
+++ twitter-clone/src/app/page.tsx
@@ -1,7 +1,14 @@
 const Home = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      
+    <div className="w-full h-full flex justify-center items-center relative bg-black">
+      <div className="max-w-screen-lg w-full h-full flex relative">
+        /* Left sidebar for navigation/header */
+        <section className="fixed w-72 flex flex-col">
+
+        </section>
+        <main>Home timeline</main>
+        <section>Right section</section>
+      </div>  
     </div>
   );
 }
