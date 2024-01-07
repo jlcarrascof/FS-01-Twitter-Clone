247916149efc19104901b866bbd980c828f5c6c7
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-01-07 12:43:53 -0400

    Approach to the first column in the main menu, changing styles base

--- twitter-clone/src/app/page.tsx
+++ twitter-clone/src/app/page.tsx
@@ -1,7 +1,8 @@
 import { BiHomeCircle, BiUser } from 'react-icons/bi';
 import { HiOutlineHashtag } from 'react-icons/hi';
-import { BsBell, BsBookmark } from 'react-icons/bs';
+import { BsBell, BsBookmark, BsTwitter } from 'react-icons/bs';
 import { HiEnvelope } from 'react-icons/hi2';
+import Link from 'next/link';
 
 const NAVIGATION_ITEMS = [
   {
@@ -35,8 +36,24 @@ const Home = () => {
     <div className="w-full h-full flex justify-center items-center relative bg-black">
       <div className="max-w-screen-lg w-full h-full flex relative">
         /* Left sidebar for navigation/header */
-        <section className="fixed w-72 flex flex-col">
-
+        <section className="fixed w-72 flex flex-col h-screen">
+          <Link href={"/"} className="my-4">
+            <BsTwitter />
+          </Link>
+          {NAVIGATION_ITEMS.map((item) => (
+            <Link 
+              className="hover:bg-white/10 transition duration-200 flex items-center justify-center space-x-2 rounded-3xl p-4"
+              href={`/${item.title.toLowerCase()}`}
+              key={item.title}
+            >
+              <div>
+                <item.icon />
+              </div>
+              <div>
+                {item.title}
+              </div>    
+            </Link>  
+          ))}  
         </section>
         <main>Home timeline</main>
         <section>Right section</section>
